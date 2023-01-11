dd12d36d734fa9af6d4b957ae6586fb5d9567073
Jeong-So <83945245+Jeong-So@users.noreply.github.com>
2023-01-12 00:08:46 +0900

    (20230111) 기초문제_출력하기_CodeUp

---
+++ Python/print_codeUp.py
@@ -0,0 +1,36 @@
+# 출력하기 1
+print("Hello")
+
+# 출력하기 2
+print("Hello World")
+
+# 출력하기 3
+print("Hello")
+print("World")
+
+print("Hello\nWorld")
+print("Hello" + '\n' + "World")
+## 줄 바꿔 출력하기 : \n을 사용하여 줄 바꿔 출력
+## print("Hello\nWorld") 와 같은 형태로도 print("Hello" + '\n' + "World") 이렇게도 가능
+
+# 출력하기 4
+print("'Hello'")
+print("""'Hello'""")
+
+# 출력하기 5
+print('"Hello World"')
+
+# 출력하기 6
+print(""""!@#$%^&*()'""")
+print("\"!@#$%^&*()\'")
+## 출력 형식에 필요한 따옴표 "" '' 와 출력할 따옴표 \' \" 로 구분
+
+# 출력하기 7
+print("\"C:\Download\\'hello'.py\"")
+print("\"C:\\Download\\\'hello\'.py\"")
+print("""\"C:\Download\\\'hello'.py\"""")
+print('\"C:\\Download\\\'hello\'.py\"')
+## 출력할 따옴표 및 특수문자 \' \" \\ 표현 + """ """ , " " , ' ' 세가지
+
+# 출력하기 8
+print("print(\"Hello\\nWorld\")")
